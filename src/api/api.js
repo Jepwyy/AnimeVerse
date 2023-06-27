176c4382cc691c56069d0caabd9d2028fb1fe997
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-06-28 02:17:54 +0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/main/',
+  baseURL: 'https://anime-verse-api.vercel.app/main/',
   withCredentials: false,
 })

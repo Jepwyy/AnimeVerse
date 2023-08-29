3a9bc1da67f49c6abda769cdaa5eff7436fdf840
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-08-29 19:03:42 +0800

    schedData

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import axios from '../api/api'
+import { useQuery } from 'react-query'
 const Schedule = () => {
+  const { data, isLoading, isError } = useQuery(['sched'], async () => {
+    const response = await axios.get(`meta/anilist/airing-schedule`)
+    return response.data
+  })
+  console.log(data)
   return (
-    <div>
+    <div className='bg-[#242424]'>
       <div>sched</div>
     </div>
   )

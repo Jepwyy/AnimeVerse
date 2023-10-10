b7e505e13c76684e20ad9d63708380b135c08dc9
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-10-10 20:47:44 +0800

    add : popular anime section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,19 @@ import Banner from '../components/Banner'
 import RecentEpisodes from '../components/RecentEpisodes'
 import TopList from '../components/TopList'
 import Release from '../components/Release'
+import Popular from '../components/Popular'
 
 const Home = () => {
   return (
     <div className='md:px-14 px-6 w-full mt-5'>
       <Banner />
       <div className=' flex lg:flex-row flex-col gap-6 mb-20 mt-8'>
-        <div className='lg:w-[75%] min-h-[45.5rem] w-full lg:order-none order-2'>
-          {/* <RecentEpisodes /> */}
+        <div className='lg:w-[75%]  w-full lg:order-none order-2'>
+          <div className='min-h-[38.5rem]'>
+            {/* <RecentEpisodes /> */}
+            <Popular />
+          </div>
+
           <div>
             <Release />
           </div>

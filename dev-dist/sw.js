efc69a42fc8b53b0c3a0d54675ca7e5e4e8b981e
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-12-23 23:51:00 +0800

    feat: return recent eps

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1rdu0atb3o8"
+    "revision": "0.qrhk2kpsiv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
